f1e3fd44b036eed5f6123b6b413ddbcc2cbd43a3
jslblar080 <jslblar080@gmail.com>
2024-06-15 01:41:45 +0900

    add main class to check autowire

--- src/main/java/com/github/jslblar080/LsApplication.java
+++ src/main/java/com/github/jslblar080/LsApplication.java
@@ -1,13 +1,26 @@
-package com.github.jslblar080.task_management_app_lesson;
+package com.github.jslblar080;
 
-import org.springframework.boot.SpringApplication;
+import com.github.jslblar080.service.impl.ProjectServiceImpl;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LsApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(LsApplication.class, args);
+//        SpringApplication.run(LsApplication.class, args);
+        try (var context = new AnnotationConfigApplicationContext("com.github.jslblar080")) {
+            ProjectServiceImpl projectRepo = context.getBean(ProjectServiceImpl.class);
+            var testIds = new Long[]{50000L, 100000L, 200000L};
+
+            for (Long testId : testIds) {
+                projectRepo.findById(testId).ifPresent(testProject -> {
+                    System.out.println("Project ID: " + testProject.getId()); // Project ID: 100000
+                    System.out.println("Project name: " + testProject.getName()); // Project name: First test
+                    System.out.println("Date created: " + testProject.getDateCreated()); // Date created: 2024-06-15
+                });
+            }
+        }
     }
 }
