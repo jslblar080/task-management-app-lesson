3893ade176244d24f74628e0d027c54205877064
jslblar080 <jslblar080@gmail.com>
2024-07-25 23:59:02 +0900

    add custom query methods to the CrudRepository extended interface

--- src/main/java/com/github/jslblar080/persistence/repository/ProjectRepository.java
+++ src/main/java/com/github/jslblar080/persistence/repository/ProjectRepository.java
@@ -3,8 +3,24 @@ package com.github.jslblar080.persistence.repository;
 import com.github.jslblar080.persistence.model.ProjectWithCrudRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 // Spring Data JPA auto-generates the implementation at runtime
 // There's no need to customize implementations of ProjectRepository
 // CrudRepository interface contains simple persistence and data access methods such as save, saveAll, findAll, findById
 public interface ProjectRepository extends CrudRepository<ProjectWithCrudRepository, Long> {
+
+    // Even for custom query methods, there's no need to write implementation
+
+    // Optional return type for the unique project with specific name
+    Optional<ProjectWithCrudRepository> findByName(String name);
+
+    // List return type for multiple projects that fit this style of criteria
+    List<ProjectWithCrudRepository> findByDateCreatedBetween(LocalDate from, LocalDate to);
+
+    List<ProjectWithCrudRepository> findByDateCreatedLessThan(LocalDate from);
+
+    List<ProjectWithCrudRepository> findByDateCreatedGreaterThan(LocalDate to);
 }
--- src/test/java/com/github/jslblar080/task_management_app_lesson/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/github/jslblar080/task_management_app_lesson/ProjectRepositoryIntegrationTest.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringJUnitConfig(classes = TestConfig.class)
@@ -30,6 +32,30 @@ public class ProjectRepositoryIntegrationTest {
         projectRepository.save(project);
         Optional<ProjectWithCrudRepository> retrievedProject = projectRepository.findById(project.getId());
         assert (retrievedProject.isPresent());
-        assert (retrievedProject.get().getName().equals(project.getName()));
+        assertEquals(retrievedProject.get().getName(), project.getName());
+    }
+
+    @Test
+    public void whenFindingProjectByName_thenOk() {
+        ProjectWithCrudRepository project = new ProjectWithCrudRepository("First test", LocalDate.now());
+        projectRepository.save(project);
+        Optional<ProjectWithCrudRepository> retrievedProject = projectRepository.findByName(project.getName());
+        assert (retrievedProject.isPresent());
+        assertEquals(retrievedProject.get().getId(), project.getId());
+    }
+
+    @Test
+    public void whenFindingProjectByDateCreatedBetween_thenOk() {
+        ProjectWithCrudRepository oldProject = new ProjectWithCrudRepository("First test", LocalDate.now().minusYears(1));
+        projectRepository.save(oldProject);
+
+        ProjectWithCrudRepository newProject = new ProjectWithCrudRepository("Second test", LocalDate.now());
+        projectRepository.save(newProject);
+
+        ProjectWithCrudRepository newProject2 = new ProjectWithCrudRepository("Third test", LocalDate.now());
+        projectRepository.save(newProject2);
+
+        List<ProjectWithCrudRepository> retrievedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1));
+        assert (retrievedProjects.size() == 2);
     }
 }
